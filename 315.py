5d3c397ddefbfe6eb7f22e63ce43a0a21537d158
Ilmira <ilmira_i.ru@mail.ru>
2023-10-31 22:35:20 +0300

    homework lesson 6

--- 315.py
+++ 315.py
@@ -1,6 +1,5 @@
 a = int(input())
 s = 0
-for i in range(a + 1):  # Между оператором и операндом ставится пробел: range(a + 1)
-	s += i**2  # i ** 2
+for i in range(a + 1):
+    s += i**2
 print(s)
-

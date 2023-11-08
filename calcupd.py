b101b6e00f7593dfabb90bf53df0bb2dc7df37db
Ilmira <ilmira_i.ru@mail.ru>
2023-11-09 00:25:08 +0300

    calc updated

--- calcupd.py
+++ calcupd.py
@@ -1,39 +1,35 @@
-import json
 import sys
-with open("json.json", "wt") as json_file:
-    json.dump(data, sys.stdout)
+with open('log.txt', 'w') as sys.stdout:
 
-def sum(x, y):
-    print(x + y)
+    def sum(x, y):
+        print(x + y)
 
+    def subtraction(x, y):
+        print(x - y)
 
-def subtraction(x, y):
-    print(x - y)
+    def multiplication(x, y):
+        print(x * y)
 
+    def division(x, y):
+        print(x / y)
 
-def multiplication(x, y):
-    print(x * y)
+    def exponent(x, y):
+        print(x ** y)
 
-
-def division(x, y):
-    print(x / y)
-
-
-def exponent(x, y):
-    print(x ** y)
-
-
-x = float(input('Hello! Enter 1st number '))
-y = float(input('Enter 2nd number '))
-z = int(input('Do you want to 1. sum, 2. subtract, 3. multiply, 4. divide, 5. exponentiate. Enter only number ',))
-if z == 1:
-    sum(x, y)
-elif z == 2:
-    subtraction(x, y)
-elif z == 3:
-    multiplication(x, y)
-elif z == 4:
-    division(x, y)
-elif z == 5:
-    exponent(x, y)
+    x = float(input('Hello! Enter 1st number '))
+    print(x)
+    y = float(input('Enter 2nd number '))
+    print(y)
+    z = int(input('Do you want to 1. sum, 2. subtract, 3. multiply, 4. divide, 5. exponentiate. Enter only number ',))
+    print(z)
+    if z == 1:
+        sum(x, y)
+    elif z == 2:
+        subtraction(x, y)
+    elif z == 3:
+        multiplication(x, y)
+    elif z == 4:
+        division(x, y)
+    elif z == 5:
+        exponent(x, y)
 
